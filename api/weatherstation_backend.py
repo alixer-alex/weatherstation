d92661dd2bd6762b12bfaf8ad206c13935f6b40d
Alex Zhuang <azhuang3@uci.edu>
2024-09-11 01:58:24 -0400

    moved build

--- api/weatherstation_backend.py
+++ api/weatherstation_backend.py
@@ -2,7 +2,7 @@ from flask import Flask, request
 import time
 from weatherstation_classes import nominatim_internet, nws_internet
 from project3 import parse_query
-app = Flask(__name__, static_folder='../build', static_url_path='/')
+app = Flask(__name__, static_folder='./build', static_url_path='/')
 
 @app.route('/')
 def index():
