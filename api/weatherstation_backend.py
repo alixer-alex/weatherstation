1407850ce4ad9714a5aae46f5fe5441fb5a5ed50
Alex Zhuang <azhuang3@uci.edu>
2024-09-12 16:23:28 -0400

    updated website for c to f, brat album

--- api/weatherstation_backend.py
+++ api/weatherstation_backend.py
@@ -4,6 +4,7 @@ from weatherstation_classes import nominatim_internet, nws_internet
 from project3 import parse_query
 app = Flask(__name__, static_folder='./build', static_url_path='/')
 
+
 @app.route('/')
 def index():
     return app.send_static_file('index.html')
@@ -17,18 +18,37 @@ def get_Weather_data():
         location= nominatim_internet(location)
         location.communication()
         location.interpret()
-        temp = get_weather(location.longitude,location.latitude)
-        
-        while(True):
-            if(temp[len(temp)-1]!="0" and temp[len(temp)-1]!="."):
-                break
-            elif(temp[len(temp)-1]=="." ):
-                temp=temp[:len(temp)-1]
-                break
-            else:
-                temp=temp[:len(temp)-1]
-        print(temp)
-        return{'temp': temp }, 200
+        tempf,temp2,tempc,tempc2 = get_weather(location.longitude,location.latitude)
+
+        for i in range(4):
+            if(i==0):
+                temp=tempf
+            elif (i==1):
+                temp=temp2
+            elif(i==2):
+                temp=tempc
+            elif(i==3):
+                temp=tempc2
+            while(True):
+                if(temp[len(temp)-1]!="0" and temp[len(temp)-1]!="."):
+                    break
+                elif(temp[len(temp)-1]=="." ):
+                    temp=temp[:len(temp)-1]
+                    break
+                else:
+                    temp=temp[:len(temp)-1]
+            if(i==0):
+                tempf=temp
+            elif (i==1):
+                temp2=temp
+            elif(i==2):
+                tempc=temp
+            elif(i==3):
+                tempc2=temp
+
+     
+        print(temp2, tempc, tempc2)
+        return{'temp': temp, 'tempmin':temp2 , 'tempc' : tempc , 'tempcmin':tempc2 }, 200
 
 def get_weather(long,lat):
     time.sleep(1)
@@ -36,6 +56,11 @@ def get_weather(long,lat):
     instance.communication()
     instance.interpret()
     query="TEMPERATURE AIR F 24 MAX"
+    query2 = "TEMPERATURE AIR F 24 MIN"
+    queryc="TEMPERATURE AIR C 24 MAX"
+    queryc2="TEMPERATURE AIR C 24 MIN"
     instance=parse_query(query, instance.response)
-    
-    return instance[0]
+    instance2=parse_query(query2, instance.response)
+    instancec=parse_query(queryc, instance.response)
+    instancec2=parse_query(queryc2, instance.response)
+    return instance[0], instance2[0], instancec[0], instancec2[0]
--- src/App.jsx
+++ src/App.jsx
@@ -5,38 +5,76 @@ import './App.css';
 
 
 function App() {
+  const [currentF, setCurrentF] = useState(true)
   const [currentTemp, setCurrentTemp]= useState(0);
+  const [currentTempMin, setCurrentTempMin]= useState(0);
+  const [currentTempC, setCurrentTempC]= useState(0);
+  const [currentTempCMin, setCurrentTempCMin]= useState(0);
   const [currentLoc, setCurrentLoc] = useState("Great Neck New York");
+
+
   const onChange=(e) => {
     e.preventDefault();
     setCurrentLoc(e.target.location.value);
   };
   useEffect(()=>{
-    setCurrentTemp(0);
+    setCurrentTemp(360);
+    setCurrentTempMin(365);
+    setCurrentTempC(360);
+    setCurrentTempCMin(365);
     fetch('/api/weather?location='+ currentLoc)
   .then(res=>res.json()
   .then(data=>{
     
     setCurrentTemp(data.temp);
+    setCurrentTempMin(data.temp2);
+    setCurrentTempC(data.tempc);
+    setCurrentTempCMin(data.tempc2);
   }))}
   ,[currentLoc])
 
 
+  function WeatherType (){
+    if(currentF == true) 
+      {
+        return <div className="weather"> 
+          <p> the highest temp in {currentLoc} in the next 24 hours is {currentTemp} F</p>
+          <p> the lowest temp in {currentLoc} in the next 24 hours is {currentTempMin} F</p>
+        </div>
+      }
+    else {
+      return <div className="weather"> 
+      <p> the highest temp in {currentLoc} in the next 24 hours is {currentTempC} non-american units (C)</p>
+      <p> the lowest temp in {currentLoc} in the next 24 hours is {currentTempCMin} non-american units (C)</p>
+    </div>
+    }
+  }
+
+  function ButtonChange(){
+    if(currentF == true) 
+      {
+        return <button className="canada" type="button" onClick={()=>setCurrentF(false)}> c </button>;
+      }
+    else{
+        return <button className="canada" type="button" onClick={()=>setCurrentF(true)}> f </button>;
+    }
+  }
 
   return (
     <>
-    <a className="me" href="https://tenors-website.vercel.app/">Me</a>
+    <a className="me" href="https://tenors-website.vercel.app/">me</a>
+
+    <ButtonChange/>
     <div>
       <form className="input" onSubmit={(e)=>onChange(e)}> 
-        <label htmlFor="Location">Location:</label><br/>
+        <label htmlFor="location">location:</label><br/>
         <input type="text" name="location"/>
-        <input className="butto" type="submit" value="Submit." />
+        <input className="butto" type="submit" value="submit." />
       </form>
 
     </div>
-    <div className="weather"> 
-      <p> The highest temp in {currentLoc} in the next 24 hours is {currentTemp} F</p>
-    </div>
+    <WeatherType/>
+    <iframe className="brat" src="https://open.spotify.com/embed/album/2lIZef4lzdvZkiiCzvPKj7?utm_source=generator" width="50%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
     </>  
   );
 }
